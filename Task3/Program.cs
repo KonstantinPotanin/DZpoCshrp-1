490fcffceea575379f24698c94f1ba9906ac0db4
Konstantin <pkv90@yandex.ru>
2023-01-29 14:18:40 +0500

    Решение задачи 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+void EvenNumber (int num)
+{
+    if (num % 2 == 0)
+    Console.WriteLine ("Число четное");
+    
+    else Console.WriteLine("Число нечетное");
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+EvenNumber (number);
