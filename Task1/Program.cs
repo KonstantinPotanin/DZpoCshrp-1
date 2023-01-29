05333bffe649a1ecca9afc064d07e57421193cae
Konstantin <pkv90@yandex.ru>
2023-01-29 13:48:01 +0500

    Рашение задачи 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+void LangeNumber (int num1, int num2)
+{
+    if (num1 == num2)
+    Console.WriteLine ("Числа равны");
+    else if (num1 > num2)
+    Console.WriteLine ("Первое число больше второго");
+    else Console.WriteLine("Первое число меньше второго");
+}
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+LangeNumber (num1, num2);
